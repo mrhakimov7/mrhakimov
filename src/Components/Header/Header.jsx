726f5a01a7cebe99a4b921aa1dcc31d84a30ee35
mrhakimov7 <hakimovabdurazzoq2008@gmail.com>
2023-10-13 18:02:17 +0500

    a

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.scss'
+
+function Header() {
+  return (
+    <div className='Header'>
+        <div className="container">
+            <nav>
+                <h2>Portfolio</h2>
+                <div>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+  )
+}
+
+export default Header
