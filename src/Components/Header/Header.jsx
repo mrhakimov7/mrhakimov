4d17685bcb104e731d5bbd1c34ad5224dd6487a2
mrhakimov7 <hakimovabdurazzoq2008@gmail.com>
2023-10-23 16:17:29 +0500

    a

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -16,19 +16,27 @@ function Header() {
           window.removeEventListener("scroll", handleScroll);
         };
       }, []);
+
+      
   return (
     <div className='Header'>
           <nav className='navb'>
                 <h2>Portfolio</h2>
-                <div>
+                <div className='navbar'>
                     <ul>
                         <li>Home</li>
                         <li>About</li>
                         <li>Service</li>
                         <li>Contact</li>
                     </ul>
+                    <span className='menu'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16" >
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                    </span>
                 </div>
             </nav>
+            
         <div className="container">
             <div className="aboutme">
             <div className="square">
